01b5be66f8c7e37183817e6777990f3e9b1eb7c0
banmtG <banmtG@gmail.com>
2024-12-15 07:25:31 +0700

    initialise

---
+++ js/viewport.js
@@ -0,0 +1,20 @@
+function updateViewportHeight() {
+    const root = document.documentElement;
+    root.style.setProperty('--viewport-height', `${window.innerHeight}px`);
+  }
+  
+  window.addEventListener('resize', updateViewportHeight);
+  window.addEventListener('orientationchange', updateViewportHeight);
+  
+  // Initial call
+  updateViewportHeight();
+
+if (window.visualViewport) {
+    window.visualViewport.addEventListener('resize', () => {
+      document.documentElement.style.setProperty(
+        '--viewport-height',
+        `${window.visualViewport.height}px`
+      );
+    });
+}
+  
