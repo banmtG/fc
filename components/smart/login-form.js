ff3f59bc684e8725f803af0b240bd06a83ba7d83
banmtG <banmtG@gmail.com>
2025-11-13 20:45:48 +0700

    2

--- components/smart/login-form.js
+++ components/smart/login-form.js
@@ -166,7 +166,7 @@ startOAuthFlow(provider) {
         window.removeEventListener('message', messageHandler);
 
         // Close the popup window (user has finished login)
-        popup.close(); //replaced with window.close(); in login.php server side
+        // popup.close(); //replaced with window.close(); in login.php server side
 
         // 3. Do a background session check with your backend
         //    This fetch call goes to session-check.php
